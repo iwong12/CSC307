f00568eb4e450b4ea12f641d551c7aa915d7f4f8
Ian Wong <iwong12@calpoly.edu>
2025-01-19 16:46:30 -0800

    IE2 part 6

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,11 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
+const addUser = (user) => {
+    users["users_list"].push(user);
+    return user;
+};
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
@@ -68,6 +73,12 @@ app.get("/users/:id", (req, res) => {
     } else {
         res.send(result)
     }
+});
+
+app.post("/users", (req, res) => {
+    const userToAdd = (req.body);
+    addUser(userToAdd);
+    res.send();
 });
 
 app.listen(port, () => {
