da3261e2f7f222e1d165a6ac719487ec81d3feb3
Ian Wong <iwong12@calpoly.edu>
2025-01-20 23:59:18 -0800

    IE2 part 7

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -43,6 +43,12 @@ const findUserByName = (name) => {
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
 
+const findUserByNameAndJob = (name, job) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name && user["job"] === job
+    );
+};
+
 const addUser = (user) => {
     users["users_list"].push(user);
     return user;
@@ -78,6 +84,18 @@ app.get("/users/:id", (req, res) => {
         res.status(404).send("Resource not found.");
     } else {
         res.send(result)
+    }
+});
+
+app.get("/users", (req, res) => {
+    const name = req.query.name;
+    const job = req.query.job;
+    if (name != undefined && job != undefined) {
+        let result = findUserByNameAndJob(name, job);
+        result = { users_list: result };
+        res.send(result);
+    } else {
+        res.send(users);
     }
 });
 
