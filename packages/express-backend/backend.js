8ee681de873d465219fbc67f20d68e8860584cc9
Ian Wong <iwong12@calpoly.edu>
2025-01-29 09:47:28 -0800

    IE3 up to part 2

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,5 +1,6 @@
 // backend.js
 import express from "express";
+import cors from "cors";
 
 const app = express();
 const port = 8000;
@@ -60,6 +61,7 @@ const delUser = (user) => {
     return user;
 };
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
